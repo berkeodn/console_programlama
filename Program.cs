18515be8d1362f14e11c926acf6ac99f1bb7ef5d
Berke ÖDEN <53332511+berkeodn@users.noreply.github.com>
2022-01-25 15:48:32 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿
+Console.WriteLine("Hello, World!");
+Console.WriteLine("İsminizi giriniz.");
+string name = Console.ReadLine();
+Console.WriteLine("Soyadınızı giriniz.");
+string surname = Console.ReadLine();
+
+Console.WriteLine("Merhaba " + name + " " + surname);
